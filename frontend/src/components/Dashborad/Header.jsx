487274f040b75fef9ac0f303e2b00e49c7391c24
Abhishek <21bcs116@nith.ac.in>
2024-01-01 12:12:01 +0530

    changes

--- frontend/src/components/Dashborad/Header.jsx
+++ frontend/src/components/Dashborad/Header.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import CustomText from "./CustomText";
+import Link from 'react-dom'
 const categoriesData = [
   {
     name: "Hotels",
     color: "bg-#bada55",
     image: "/hotel.png",
+    link: "/hotels",
   },
   {
     name: "Home Stays",
     color: "bg-#ffe4e1",
     image: "/home.png",
+    link: "/home-stays",
   },
   {
     name: "Adventure Activities",
     color: "bg-#ffc0cb",
     image: "/travel..png",
+    link: "/adventure-activities",
   },
   {
     name: "Jobs",
     color: "bg-#c6e2ff",
     image: "/job.png",
+    link: "/jobs",
   },
 ];
 
@@ -30,15 +35,20 @@ const Header = () => {
 
       <div className=" mx-3 grid grid-cols-2 gap-y-1 overflow-hidden max-w-[100%] m-auto">
         {categoriesData.map((category, index) => (
-          <div className="flex flex-col  justify-center items-center  rounded-md bg-transparent " key={category.name}>
+          // <Link>
             <div
+              className="flex flex-col  justify-center items-center  rounded-md bg-transparent "
               key={category.name}
-              className="flex flex-col justify-center items-center bg-[#f5f5f5] border border-[#e9e4e4a9]  rounded-lg shadow-sm "
             >
-              <img src={category.image} alt={category.name} width={160} />
+              <div
+                key={category.name}
+                className="flex flex-col justify-center items-center bg-[#f5f5f5] border border-[#e9e4e4a9]  rounded-lg shadow-sm "
+              >
+                <img src={category.image} alt={category.name} width={160} />
+              </div>
+              <p className="text-[13px] font-[500] mt-1 ">{category.name}</p>
             </div>
-            <p className="text-[13px] font-[500] mt-1 ">{category.name}</p>
-          </div>
+          // </Link>
         ))}
       </div>
     </div>
